7ec1e9592da68d4373d6d2843ae3b7070be3cc81
jose <jose.sanchez.simon@gmail.com>
2017-12-07 18:39:15 +0100

    Change proyect to use scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
