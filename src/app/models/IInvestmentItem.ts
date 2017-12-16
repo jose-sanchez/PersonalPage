eb25fd22f26401819bc14e79a6b58158691fd35c
jose <jose.sanchez.simon@gmail.com>
2017-12-16 13:49:19 +0100

    creade model to represent a investment item

---
+++ src/app/models/IInvestmentItem.ts
@@ -0,0 +1,11 @@
+export interface IInvestmentItem{
+    id:number,
+    investmentType:string,
+    name:string,
+    initialInvestment:number,
+    totalInvestment:string,
+    dividents:number,
+    totaldividents:string,
+    reinvestdividents:boolean,
+    years:number
+}
---
+++ src/app/models/index.ts
@@ -0,0 +1,1 @@
+export {IInvestmentItem} from './IInvestmentItem';
