2585215c877e5dda3bf6d9b45f675cdd4a00ec86
jose <jose.sanchez.simon@gmail.com>
2017-12-17 16:23:02 +0100

    mock data for the list of investment

--- src/app/investment-portfolio/investment-portfolio.component.ts
+++ src/app/investment-portfolio/investment-portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IInvestmentItem } from '../models/IInvestmentItem';
 
 @Component({
   selector: 'app-investment-portfolio',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./investment-portfolio.component.scss']
 })
 export class InvestmentPortfolioComponent implements OnInit {
+  investment :IInvestmentItem;
+  private investmentList: IInvestmentItem[];
+  
+  constructor() {
+    this.investment= {id:1,dividents:0,initialInvestmentAmount:0,initialInvestmentPrice:0,investmentType:"t",name:"Roche",reinvestdividents:true,totaldividents:"0",totalInvestment:"0",years:0};
+    this.investmentList = [];
+    this.investmentList.push(this.investment);
+    this.investmentList.push(this.investment);
+    this.investmentList.push(this.investment);
 
-  constructor() { }
+
+   }
 
   ngOnInit() {
   }
