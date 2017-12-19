87e4c754a4594d51a23275bc8ee2f80a9bf9113a
jose <jose.sanchez.simon@gmail.com>
2017-12-19 22:21:35 +0100

    Add Redux to retrieve list of investmentitems

--- src/app/investment-portfolio/investment-portfolio.component.ts
+++ src/app/investment-portfolio/investment-portfolio.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { IInvestmentItem } from '../models/IInvestmentItem';
-
+import { Observable } from 'rxjs';
+import {NgRedux,select} from 'ng2-redux';
+import { InvestmentActions } from './investment-portfolio.actions';
+import {IAppState } from '../store'
 @Component({
   selector: 'app-investment-portfolio',
   templateUrl: './investment-portfolio.component.html',
   styleUrls: ['./investment-portfolio.component.scss']
 })
 export class InvestmentPortfolioComponent implements OnInit {
-  investment :IInvestmentItem;
-  private investmentList: IInvestmentItem[];
-  
-  constructor() {
-    this.investment= {id:1,dividents:0,initialInvestmentAmount:0,initialInvestmentPrice:0,investmentType:"t",name:"Roche",reinvestdividents:true,totaldividents:"0",totalInvestment:"0",years:0};
-    this.investmentList = [];
-    this.investmentList.push(this.investment);
-    this.investmentList.push(this.investment);
-    this.investmentList.push(this.investment);
-
+ 
+  @select((state)=> state.investments) investmentList$: Observable<IInvestmentItem>
+  constructor(private ngRedux:NgRedux<IAppState>,private investmentListActions:InvestmentActions) {
 
    }
 
   ngOnInit() {
+        //with NG redux -- This action will refresh the content of all exercise variables
+        this.investmentListActions.getInvestments();
+        console.log(this.investmentList$.count);
   }
 
 }
