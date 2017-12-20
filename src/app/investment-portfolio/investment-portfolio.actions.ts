72ab437eb1acb1a50cc9bac6c681b243d8c1f686
jose <jose.sanchez.simon@gmail.com>
2017-12-20 18:39:24 +0100

    Adding remove Investment item action

--- src/app/investment-portfolio/investment-portfolio.actions.ts
+++ src/app/investment-portfolio/investment-portfolio.actions.ts
@@ -2,7 +2,9 @@ import {NgRedux} from 'ng2-redux';
 import {IAppState} from '../store';
 import {Injectable} from '@angular/core';
 import { InvestmentService } from '../investment.service';
+import { IInvestmentItem} from '../models/index';
 export const GET_INVESTMENTS = 'investments/GET'
+export const REMOVE_INVESTMENTS = 'investments/REMOVE'
 
 @Injectable()
 export class InvestmentActions{
@@ -18,6 +20,13 @@ export class InvestmentActions{
             type:GET_INVESTMENTS,
             investments
           });
+      });
+  }
+
+  public RemoveInvestment(investment:IInvestmentItem){
+    this.ngRedux.dispatch({
+        type:REMOVE_INVESTMENTS,
+        investment
       });
   }
 }
